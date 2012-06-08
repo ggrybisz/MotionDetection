c51e8a483220909605625b63c69cf130f5c4099a
Grzegorz Grybisz <ggrybisz@gmail.com>
2012-06-08 13:50:39 +0200

    basic interface and file opening

---
+++ MotionDetection/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+
+namespace MotionDetection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Uri filePath = new Uri(dialog.FileName);
+                mainMovieMediaElement.Source = filePath;
+                mainMovieMediaElement.IsMuted = true;
+                mainMovieMediaElement.LoadedBehavior = MediaState.Play;
+
+                BitmapImage img = new BitmapImage();
+                
+            }
+        }
+    }
+}
