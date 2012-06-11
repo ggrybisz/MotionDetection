144a8d37a81f9935d37546341ec220e04e6dffad
Grzegorz Grybisz <ggrybisz@gmail.com>
2012-06-11 18:12:37 +0200

    first primitive detection implemented

--- MotionDetection/MainWindow.xaml.cs
+++ MotionDetection/MainWindow.xaml.cs
@@ -1,17 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
+using System.Windows.Forms;
+using AForge.Video.FFMPEG;
+using System.Drawing;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Windows.Forms;
+using System.Collections.Concurrent;
+using System.Threading;
 
 
 namespace MotionDetection
@@ -21,23 +17,85 @@ namespace MotionDetection
     /// </summary>
     public partial class MainWindow : Window
     {
+        VideoProcessor processor;
+        VideoFileReader videoReader;
+
+        Thread processMovieThread;
+        Thread updateImagesThread;
+
+        BlockingCollection<BitmapSource> diffrencePictures = new BlockingCollection<BitmapSource>();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            processMovieThread = new Thread(new ThreadStart(processMovie));
+            processMovieThread.Name = "PROCESS MOVIE THREAD";
+
+            updateImagesThread = new Thread(new ThreadStart(updateScreens));
+            updateImagesThread.Name = "UPDATE IMAGES THREAD";
+        }
+
+        private void processMovie()
+        {
+            if (videoReader.IsOpen)
+            {
+                Bitmap background = videoReader.ReadVideoFrame();
+  
+                Bitmap videoFrame;
+
+                while((videoFrame = videoReader.ReadVideoFrame())!=null)
+                {
+                    Bitmap diff = processor.findDifference(background, videoFrame);
+
+                  //diffrencePictures.Add(VideoProcessor.convertBitmap(diff));
+                  
+                    this.Dispatcher.Invoke(new Action(() => differenceImage.Source = VideoProcessor.convertBitmap(diff)));
+                    
+                    Bitmap final = processor.showMotion(background, diff);
+                    this.Dispatcher.Invoke(new Action(() => finalImage.Source = VideoProcessor.convertBitmap(final)));
+                    
+                    videoFrame.Dispose();
+                    diff.Dispose();
+                    final.Dispose();
+                    System.Console.Out.WriteLine("PROCESS MOVIE frame " + diffrencePictures.Count);
+                }
+                background.Dispose();
+                videoReader.Close();
+                videoReader.Dispose();
+            }
+        }
+
+        private void updateScreens()
+        {
+            System.Console.Out.WriteLine("UPDATE SCREEN START");
+            while (true)
+            {
+                
+                BitmapSource item = diffrencePictures.Take(); //jeśli kolejka jest pusta, blokuje wątek
+                item.Dispatcher.Invoke(new Action(() => differenceImage.Source = item));
+                System.Console.Out.WriteLine("UPDATE SCREEN LOOP");
+            }
         }
 
         private void OpenFile_Click(object sender, RoutedEventArgs e)
         {
+            processor = new VideoProcessor();
+            videoReader = new VideoFileReader();
+
             OpenFileDialog dialog = new OpenFileDialog();
+
             if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                Uri filePath = new Uri(dialog.FileName);
-                mainMovieMediaElement.Source = filePath;
-                mainMovieMediaElement.IsMuted = true;
-                mainMovieMediaElement.LoadedBehavior = MediaState.Play;
+                Uri path = new Uri(dialog.FileName);
+                videoReader.Open(path.OriginalString);
 
-                BitmapImage img = new BitmapImage();
-                
+                originalMediaElement.Source = path;
+                originalMediaElement.IsMuted = true;
+                originalMediaElement.LoadedBehavior = MediaState.Play;
+
+                processMovieThread.Start();
+               // updateImagesThread.Start();
             }
         }
     }
---
+++ MotionDetection/VideoProcessor.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AForge.Imaging.Filters;
+using System.Drawing;
+using AForge.Video;
+using System.Windows.Media.Imaging;
+using System.Windows;
+using System.Drawing.Imaging;
+using AForge.Imaging;
+
+namespace MotionDetection
+{
+    class VideoProcessor
+    {  
+       // Bitmap backgroundFrame;
+        Difference difference = new Difference();
+        IFilter threshold = new Threshold(15);
+        Grayscale grayscaleFilter = new Grayscale(0.2125, 0.7154, 0.0721);
+        Erosion erosionFilter = new Erosion();
+
+        public VideoProcessor()
+        {
+            
+        }
+
+        public static BitmapSource convertBitmap(System.Drawing.Bitmap source)
+        {
+            IntPtr ip = source.GetHbitmap();
+            BitmapSource bs = null;
+
+            bs = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(ip,
+               IntPtr.Zero, Int32Rect.Empty,
+               System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+            return bs;
+        }
+
+        public Bitmap findDifference(Bitmap background, Bitmap currentFrame)
+        {
+            //Przekształcenie w obrazy czarno-białe
+            
+            background = grayscaleFilter.Apply(background);
+            currentFrame = grayscaleFilter.Apply(currentFrame);
+
+            difference.OverlayImage = background;
+            
+            //różnica pomiędzy obrazami
+            Bitmap tmp1 = difference.Apply(currentFrame);
+            //przekształcenie na obraz binarny
+            Bitmap tmp2 = threshold.Apply(tmp1);
+
+            //Usunięcie szumu
+            Bitmap tmp3 = erosionFilter.Apply(tmp2);
+            tmp1.Dispose();
+            tmp2.Dispose();
+            return tmp3;
+        }
+
+        public Bitmap showMotion(Bitmap originalImage, Bitmap motionImage)
+        {
+            // czerwony kanał z oryginalnego obrazu
+            IFilter extrachChannel = new ExtractChannel(RGB.R);
+            Bitmap redChannel = extrachChannel.Apply(originalImage);
+            
+            //  merge red channel with motion regions
+            Merge mergeFilter = new Merge();
+            mergeFilter.OverlayImage = motionImage;
+            Bitmap tmp4 = mergeFilter.Apply(redChannel);
+            // replace red channel in the original image
+            ReplaceChannel replaceChannel = new ReplaceChannel(RGB.R, tmp4);
+           // replaceChannel.ChannelImage = tmp4;
+            Bitmap tmp5 = replaceChannel.Apply(originalImage);
+
+            redChannel.Dispose();
+            tmp4.Dispose();
+            return tmp5;
+        }
+    }
+}
